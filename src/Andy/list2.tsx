56ed707bbd30a6cb777491be439b27fe703d8565
andysantos04 <andysantocv@gmail.com>
2025-04-02 21:42:19 -0400

    info added

---
+++ src/Andy/list2.tsx
@@ -0,0 +1,13 @@
+function AndysList(){
+    return(
+        <ol>
+            <li>Computer</li>
+            <li>Barbell</li>
+            <li>Dumbbell</li>
+            <li>Controller</li>
+            <li>Cellphone</li>
+        </ol>
+    )
+}
+
+export default AndysList;
---
+++ src/Andy/table2.tsx
@@ -0,0 +1,37 @@
+function AndysTable(){
+    return(
+        <table className="collapse">
+            <caption>WPI Academic Record</caption>
+            <thead>
+            <tr>
+                <th>Courses</th>
+                <th>Grade</th>
+                <th>Credits Earned</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Calculus IV</td>
+                <td>A</td>
+                <td>3</td>
+            </tr>
+            <tr>
+                <td>Software Engineering</td>
+                <td>A</td>
+                <td>3</td>
+            </tr>
+            <tr>
+                <td>Cell Biology</td>
+                <td>A</td>
+                <td>3</td>
+            </tr>
+            <tr>
+                <td>Modern Physics</td>
+                <td>A</td>
+                <td>3</td>
+            </tr>
+            </tbody>
+        </table>
+    )
+}
+export default AndysTable;
