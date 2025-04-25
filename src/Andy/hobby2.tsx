1955e542d5ee8b8c1e77eb0e2a0452112331a859
andysantos04 <andysantocv@gmail.com>
2025-04-24 23:39:43 -0400

    missing code implemented

--- src/Andy/hobby2.tsx
+++ src/Andy/hobby2.tsx
@@ -3,15 +3,31 @@
 //     b. table1.tsx component (table from Assignment 1b)
 //     c. form.tsx component (form from Assignment 1b)
 
+
+import AndysList from "./list2.tsx";
+import AndysTable from "./table2.tsx";
+import AndysForm from "./form2.tsx";
 function AndysHobby() {
     return (
         <div>
+
+            <h2>Andy's Hobbies</h2>
+            <div id="rc">
+                <img src="src/images/buffguy.jpg" width="640"/>
+            </div>
             <p className = "andyhobby">
                 My favorite hobbies are play video games, watch movies, go out with my girlfriend, and workout at the
                 gym.
             </p>
 
+            <div id="andystuff">
+                <AndysList/>
+                <AndysTable/>
 
+            </div>
+
+            <br/>
+            <AndysForm/>
             <hr/>
 
         </div>
